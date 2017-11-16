c41fdfb12f72e21fe1e693400f5702b4451b901a
Julio de la Vega <j_dlvr@hotmail.com>
2017-11-16 13:02:16 +0100

    Added specs for app and header component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,54 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { APP_BASE_HREF } from '@angular/common';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { DropdownDirective } from './shared/dropdown.directive';
+import { shoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeService } from './recipe-book/recipe.service';
+import { DataStorageService } from './shared/data-storage.service';
+import { AppRoutingModule } from './app-routing.module';
+import { RecipeSelectComponent } from './recipe-book/recipe-select/recipe-select.component';
+import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.component';
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [ 
+        RouterTestingModule, 
+        HttpModule, 
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AppRoutingModule,
+        ReactiveFormsModule ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        ShoppingListComponent,
+        RecipeBookComponent,
+        RecipeListComponent,
+        RecipeItemComponent,
+        RecipeDetailComponent,
+        ShoppingListEditComponent,
+        DropdownDirective,
+        RecipeSelectComponent,
+        RecipeEditComponent
       ],
+      providers: [shoppingListService, RecipeService, DataStorageService, {provide: APP_BASE_HREF, useValue : '/' }],
     });
     TestBed.compileComponents();
   });
@@ -17,16 +59,26 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
+  describe('regarding the view', () => {
+    it('should render the app-header', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('app-header')).not.toBeNull()
+    }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+    it('should render the app container', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('div.container')).not.toBeNull()
+    }));
+
+    it('should render the router-outlet within app container', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('div.container router-outlet')).not.toBeNull()
+    }));
+  });
 });
--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,14 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
+import { HttpModule } from '@angular/http';
+import 'rxjs/add/observable/of';
 
 import { HeaderComponent } from './header.component';
+import { DataStorageService } from './../shared/data-storage.service';
+import { RecipeService } from './../recipe-book/recipe.service';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
-
+  let dataStorageService: DataStorageService;
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      imports: [HttpModule],
+      declarations: [ HeaderComponent ],
+      providers: [DataStorageService, RecipeService]
     })
     .compileComponents();
   }));
@@ -19,7 +27,66 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create header component', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('regarding the component methods', () => {
+    describe('.fetchData', () => {
+      beforeEach(() => {
+        dataStorageService = fixture.debugElement.injector.get(DataStorageService);
+        spyOn(dataStorageService, 'getRecipes')
+      });
+
+      it('should get recipes', () => {
+        component.fetchData();
+        expect(dataStorageService.getRecipes).toHaveBeenCalled();
+      });
+    });
+
+     describe('.onSaveData', () => {
+      beforeEach(() => {
+        dataStorageService = fixture.debugElement.injector.get(DataStorageService);
+        spyOn(dataStorageService, 'storeRecipes').and.returnValue(Observable.of('OK'));
+      });
+
+      it('should store recipes', () => {
+        component.onSaveData();
+        expect(dataStorageService.storeRecipes).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('regarding the view', () => {
+    it('should be a navbar', () => {
+      const fixture = TestBed.createComponent(HeaderComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('nav.navbar')).not.toBeNull()
+    });
+
+    it('should have home hyperlink', () => {
+      const fixture = TestBed.createComponent(HeaderComponent);
+      fixture.detectChanges();
+      const homeHyperLink = fixture.debugElement.nativeElement.querySelector('a[name="home"]');
+      expect(homeHyperLink).not.toBeNull();
+      expect(homeHyperLink.getAttribute("routerLink")).toBe('/');
+    });
+
+    it('should have recipes hyperlink', () => {
+      const fixture = TestBed.createComponent(HeaderComponent);
+      fixture.detectChanges();
+      const homeHyperLink = fixture.debugElement.nativeElement.querySelector('a[name="recipes-link"]');
+      expect(homeHyperLink).not.toBeNull();
+      expect(homeHyperLink.getAttribute("routerLink")).toBe('/recipes');
+    });
+
+    it('should have shopping list hyperlink', () => {
+      const fixture = TestBed.createComponent(HeaderComponent);
+      fixture.detectChanges();
+      const homeHyperLink = fixture.debugElement.nativeElement.querySelector('a[name="shopping-list-link"]');
+      expect(homeHyperLink).not.toBeNull();
+      expect(homeHyperLink.getAttribute("routerLink")).toBe('/shopping-list');
+    });
   });
 });
